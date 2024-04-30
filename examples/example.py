ca1c72325bd6237249b0a80ed7df79b0f6c44241
akt1215 <125686712+akt1215@users.noreply.github.com>
2024-04-30 16:09:56 +0000

    Updates

--- examples/example.py
+++ examples/example.py
@@ -20,13 +20,21 @@ print(info)
 
 def callback(data):
     print(data)
-    for i in range(len(data["data"])):
-        with open('data.csv', mode='a') as data_file:
-            data_writer = csv.writer(data_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-            data_writer.writerow(data["data"][i])
+    with open('raw_data.txt', 'a') as file:
+        file.write(str(data) + '\n')
+    
+    # for i in range(len(data["data"])):
+    #     with open('grab2.csv', mode='a') as data_file:
+    #         data_writer = csv.writer(data_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    #         data_writer.writerow(data["data"][i])
 
-unsubscribe = neurosity.brainwaves_raw(callback)
-time.sleep(2)
-neurosity.add_marker("eyes-closed")
-time.sleep(2)
+    for i in range(len(data["psd"])):
+        with open('psd.csv', mode='a') as data_file:
+            data_writer = csv.writer(data_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            data_writer.writerow(data["psd"][i])
+
+
+# unsubscribe = neurosity.brainwaves_raw(callback)
+unsubscribe = neurosity.brainwaves_psd(callback)
+time.sleep(10)
 unsubscribe()
