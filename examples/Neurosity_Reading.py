6c1e7df9ed7260aab5961d828e8fdb52a0b209d8
akt1215 <125686712+akt1215@users.noreply.github.com>
2024-05-12 01:07:27 +0000

    Updated

--- examples/Neurosity_Reading.py
+++ examples/Neurosity_Reading.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 import os
 import time
 import csv
+import random
 
 load_dotenv()
 
@@ -19,25 +20,28 @@ info = neurosity.get_info()
 print(info)
 timeStart = time.time()
 marker = 0
+split = 5
 
 def callback(data):
     global timeStart  # Declare timeStart as a global variable
     global marker  # Declare marker as a global variable
-    with open('demo.csv', mode='a') as data_file:
-            data_writer = csv.writer(data_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-            data_writer.writerow(marker)
-    print(".")
-    print(round((time.time() - timeStart) % 5))
-    if round((time.time() - timeStart) % 5) == 0:
+    global split
+
+    #print(".")
+    print(round((time.time() - timeStart) % split))
+    if round((time.time() - timeStart) % split) == 0:
         time.sleep(0.5)
-        #timeStart = time.time()
-        print("action")
         marker = 1 - marker
-        # add marker to the csv file
-        with open('demo.csv', mode='a') as data_file:
-            data_writer = csv.writer(data_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-            data_writer.writerow([marker])  # Convert marker to a list
+        if marker == 0:
+            print("close")
+        else:
+            print("open")
+        split = random.randint(3, 6)
+        print(split)
+        timeStart = time.time()
+
     for i in range(len(data["psd"])):
+        data["psd"][i].append(marker)
         with open('demo.csv', mode='a') as data_file:
             data_writer = csv.writer(data_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
             data_writer.writerow(data["psd"][i])
--- examples/test.py
+++ examples/test.py
@@ -1,9 +1,29 @@
 import time
+import csv
+import random 
 
-for i in range (10):
-    print("hello")
-    time.sleep(0.1)  # Optional: Adjust the delay if needed
-    for _ in range(10):  # Print "hello" 10 times, effectively creating a delay
-        time.sleep(0.2)
-        print("hello", end=" ")
-    print("\nworld")
+data = [1,2,3,4,5]
+timeStart = time.time() # Declare timeStart as a global variable
+marker = 0 # Declare marker as a global variable
+split = 5
+
+for _ in range(40):
+    print(round((time.time() - timeStart) % split))
+    if round((time.time() - timeStart) % split) == 0:
+        time.sleep(0.5)
+        marker = 1 - marker
+        if marker == 0:
+            print("close")
+        else:
+            print("open")
+        split = random.randint(3, 6)
+        print(split)
+        timeStart = time.time()
+        
+    for i in range(len(data)):
+        data = [1,2,3,4,5]
+        data.append(marker)
+        with open('demo2.csv', mode='a') as data_file:
+            data_writer = csv.writer(data_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            data_writer.writerow(data)
+    time.sleep(0.5)
